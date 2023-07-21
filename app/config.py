aa63795d23276a10ecb3c88210c3c2bfef9c284a
RAJUKUMAR SUDDALA <97968060+SRK-RAJU@users.noreply.github.com>
2023-07-21 14:07:37 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,9 +9,9 @@ SECRET_KEY = 'gobbledygook'
 try:
     DATABASE_URL = os.environ['DATABASE_URL']
 except KeyError:
-    DATABASE_URL = "postgresql://localhost:5790/db"
+    DATABASE_URL = "postgresql://localhost:5432/db"
 
 try:
     REDIS_URL = os.environ['REDIS_URL']
 except KeyError:
-    REDIS_URL = "redis://localhost:5798/db"
+    REDIS_URL = "redis://localhost:5432/db"
